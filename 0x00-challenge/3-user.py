40ca6ba667cd1d6bff91c3efdce5ce493abe6986
SeifEleslam <seifgouda98@gmail.com>
2024-01-28 13:36:14 +0200

    task0

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -38,7 +38,7 @@ class User():
         - Hash `pwd` in MD5 before assign to `__password`
         """
         if pwd is None or type(pwd) is not str:
-            self.__password = None
+            return
         else:
             self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
